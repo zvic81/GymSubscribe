0fd66e9e2189e06a44001cfaab884a9d365a932f
vic <zvic@list.ru>
2024-03-28 12:43:54 +0300

    feat: Migration to the PostgreSQL database completed. Used spring-boot-docker-compose

--- src/main/java/com/example/gymsubscribe/TestDataLoader.java
+++ src/main/java/com/example/gymsubscribe/TestDataLoader.java
@@ -17,29 +17,31 @@ public class TestDataLoader implements CommandLineRunner {
     private final ClientRepo clientRepo;
     @Override
     public void run(String... args) throws Exception {
-        Client client = new Client();
-        client.setFirstName("Ivan");
-        client.setSecondName("Petrov");
-        client.setDateOfBirth(LocalDate.now().plusMonths(1));
-        clientRepo.save(client);
-        Client client2 = new Client();
-        client2.setFirstName("Jakob");
-        client2.setSecondName("Veinstein");
-        client2.setDateOfBirth(LocalDate.now().plusMonths(3));
-        clientRepo.save(client2);
+        if (clientRepo.findAll().isEmpty()) {
+            Client client = new Client();
+            client.setFirstName("Ivan");
+            client.setSecondName("Petrov");
+            client.setDateOfBirth(LocalDate.now().plusMonths(1));
+            clientRepo.save(client);
+            Client client2 = new Client();
+            client2.setFirstName("Jakob");
+            client2.setSecondName("Veinstein");
+            client2.setDateOfBirth(LocalDate.now().plusMonths(3));
+            clientRepo.save(client2);
 
-        Subscribe subscribe1 = new Subscribe();
-        subscribe1.setStartDate(LocalDate.now());
-        subscribe1.setEndDate(LocalDate.now().plusMonths(1));
-        subscribe1.setPrice(50.0);
-        subscribe1.setClient(client);
-        subscribeRepo.save(subscribe1);
+            Subscribe subscribe1 = new Subscribe();
+            subscribe1.setStartDate(LocalDate.now());
+            subscribe1.setEndDate(LocalDate.now().plusMonths(1));
+            subscribe1.setPrice(50.0);
+            subscribe1.setClient(client);
+            subscribeRepo.save(subscribe1);
 
-        Subscribe subscribe2 = new Subscribe();
-        subscribe2.setStartDate(LocalDate.now());
-        subscribe2.setEndDate(LocalDate.now().plusMonths(3));
-        subscribe2.setPrice(100.0);
-        subscribe2.setClient(client2);
-        subscribeRepo.save(subscribe2);
+            Subscribe subscribe2 = new Subscribe();
+            subscribe2.setStartDate(LocalDate.now());
+            subscribe2.setEndDate(LocalDate.now().plusMonths(3));
+            subscribe2.setPrice(100.0);
+            subscribe2.setClient(client2);
+            subscribeRepo.save(subscribe2);
+        }
     }
 }
